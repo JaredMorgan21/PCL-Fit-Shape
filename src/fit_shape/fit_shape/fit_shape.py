77748d187cd5d09e3d513b6123ced00e4a9878b3
JaredMorgan21 <jmmorgan1007@gmail.com>
2023-10-08 20:17:12 -0400

    added planar segmentation and fit functions. Fit functions could use some work as they tend to recognize cylindrical objects as spheres. Multiple object detection next

--- src/fit_shape/fit_shape/fit_shape.py
+++ src/fit_shape/fit_shape/fit_shape.py
@@ -64,15 +64,59 @@ class PCSubscriber(Node):
     pc = pcl.PointCloud();
     pc.from_list(pcls);
     
-    # taken from https://github.com/strawlab/python-pcl/blob/master/examples/official/Segmentation/Plane_model_segmentation.py
-    seg = pc.make_segmenter()
-    seg.set_model_type(pcl.SACMODEL_PLANE)
-    seg.set_method_type(pcl.SAC_RANSAC)
-    indices, model = seg.segment()
+    # from https://github.com/strawlab/python-pcl/blob/master/examples/official/Filtering/VoxelGrid_160.py
+    #sor = pc.make_voxel_grid_filter()
+    #sor.set_leaf_size(0.01, 0.01, 0.01)
+    #pc_filtered = sor.filter()
     
-    #print(type(seg));
-
-    pcls = pc.to_list();
+    # taken from https://github.com/strawlab/python-pcl/blob/master/examples/official/Segmentation/Plane_model_segmentation.py
+    # segments out plane
+    seg = pc.make_segmenter_normals(ksearch=50)
+    seg.set_optimize_coefficients(True)
+    seg.set_model_type(pcl.SACMODEL_NORMAL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    seg.set_distance_threshold(0.01)
+    seg.set_normal_distance_weight(0.01)
+    seg.set_max_iterations(100)
+    indices, coefficients = seg.segment()
+    
+    non_plane = pc.extract(indices, True)
+    
+    # identifies cylinder
+    seg = non_plane.make_segmenter_normals(ksearch=50)
+    seg.set_optimize_coefficients(True)
+    seg.set_model_type(pcl.SACMODEL_CYLINDER)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    seg.set_distance_threshold(0.03)
+    seg.set_normal_distance_weight(0.01)
+    seg.set_max_iterations(100)
+    cyl_indices, cyl_coefficients = seg.segment()
+    
+    # identifies sphere
+    seg = non_plane.make_segmenter_normals(ksearch=50)
+    seg.set_optimize_coefficients(True)
+    seg.set_model_type(pcl.SACMODEL_SPHERE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    seg.set_distance_threshold(0.03)
+    seg.set_normal_distance_weight(0.01)
+    seg.set_max_iterations(100)
+    sphere_indices, sphere_coefficients = seg.segment()
+    
+    cylinder_fit = len(cyl_indices)/non_plane.size * 100
+    sphere_fit = len(sphere_indices)/non_plane.size * 100
+    
+    self.get_logger().info('Cylinder fit: %f, Sphere fit: %f' % (cylinder_fit, sphere_fit))
+    
+    sphere = non_plane.extract(sphere_indices, False)
+    cylinder = non_plane.extract(cyl_indices, False)
+    
+    if sphere_fit > cylinder_fit:
+    	pcls = sphere.to_list();
+    	self.get_logger().info('Sphere Selected')
+    else:
+    	pcls = cylinder.to_list();
+    	self.get_logger().info('Cylinder Selected')
+    
     pcmsg = pc2.create_cloud_xyz32(pcmsg.header,pcls);
 
     self.publisher_.publish(pcmsg)
